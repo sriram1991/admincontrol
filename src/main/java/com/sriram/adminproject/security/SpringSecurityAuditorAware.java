b2d8eb12a4fb6ba834fbcc73de35e2a14d30d0dd
Sriram kulkarni <Sriram.shamrao74@gmail.com>
2021-09-13 23:46:03 +0530

    added java code

---
+++ src/main/java/com/sriram/adminproject/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.sriram.adminproject.security;
+
+import com.sriram.adminproject.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
---
+++ src/main/java/com/sriram/adminproject/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.sriram.adminproject.security;
