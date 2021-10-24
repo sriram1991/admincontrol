b5b808dfa6cff18dd6cdb4e6587a0dcd50f93417
Hemendra Soni <hemendrasoni.soni5@gmail.com>
2021-10-24 18:13:31 +0530

    Pancard List Component Added

--- src/main/webapp/app/account/account.route.ts
+++ src/main/webapp/app/account/account.route.ts
@@ -8,6 +8,12 @@ import { registerRoute } from './register/register.route';
 import { sessionsRoute } from './sessions/sessions.route';
 import { settingsRoute } from './settings/settings.route';
 import { pancardRoute } from './user-service/pancard/pancard.route';
+import { Account } from 'app/core/auth/account.model';
+import { UserManagementResolve, userManagementRoute } from 'app/admin/user-management/user-management.route';
+import { UserManagementComponent } from 'app/admin/user-management/list/user-management.component';
+import { UserManagementDetailComponent } from 'app/admin/user-management/detail/user-management-detail.component';
+import { UserManagementUpdateComponent } from 'app/admin/user-management/update/user-management-update.component';
+import { PanDetailManagementComponent } from './user-service/pan-detail-management/pan-detail-management.component';
 
 const ACCOUNT_ROUTES = [
   activateRoute,
@@ -25,5 +31,33 @@ export const accountState: Routes = [
     path: '',
     // component: Account,
     children: ACCOUNT_ROUTES,
+  },
+  {
+    path: 'pancard-management',
+    data: {
+      defaultSort: 'id,asc',
+    },
+    component: UserManagementComponent,
+  },
+  {
+    path: 'pancard-management/:login/view',
+    component: UserManagementDetailComponent,
+    resolve: {
+      user: UserManagementResolve,
+    },
+  },
+  {
+    path: 'pancard-management/new',
+    component: UserManagementUpdateComponent,
+    resolve: {
+      user: UserManagementResolve,
+    },
+  },
+  {
+    path: 'pancard-management/:login/edit',
+    component: UserManagementUpdateComponent,
+    resolve: {
+      user: UserManagementResolve,
+    },
   },
 ];
